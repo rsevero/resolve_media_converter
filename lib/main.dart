710898358e4942db69c68202293509657317ad16
Rodrigo Severo <rsev@pm.me>
2026-06-27 07:22:35 -0300

    Manage desktop startup window in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,31 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/widgets.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'app/app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
+    await windowManager.ensureInitialized();
+
+    const windowOptions = WindowOptions(
+      size: Size(1200, 1300),
+      minimumSize: Size(1200, 800),
+      center: true,
+      title: 'Resolve File Converter',
+    );
+
+    unawaited(
+      windowManager.waitUntilReadyToShow(windowOptions, () async {
+        await windowManager.show();
+        await windowManager.focus();
+      }),
+    );
+  }
+
   runApp(const ResolveMediaConverterApp());
 }
